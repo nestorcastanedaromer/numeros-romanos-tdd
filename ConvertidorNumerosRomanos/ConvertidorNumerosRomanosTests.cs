59a42c85af25b3992f2fbded2ca34e0519fb6c07
nestorcastanedaromer <nestor.castaneda@sinco.co>
2025-10-27 16:38:46 -0500

    🟦Si_NumeroEs1_Debe_RetornarI

--- ConvertidorNumerosRomanos/ConvertidorNumerosRomanosTests.cs
+++ ConvertidorNumerosRomanos/ConvertidorNumerosRomanosTests.cs
@@ -9,21 +9,15 @@ namespace ConvertidorNumerosRomanos.Tests
         [Fact]
         public void Si_NumeroEs1_Debe_RetornarI()
         {
-            var calculadora = new CalculadoraNumerosRomanos();
-
-            var numeroRomano= calculadora.Calcular("1");
+            string numeroRomano = CalculadoraNumerosRomanos.Calcular("1");
 
             numeroRomano.Should().Be("I");
         }
     }
 
-    internal class CalculadoraNumerosRomanos
+    internal static class CalculadoraNumerosRomanos
     {
-        public CalculadoraNumerosRomanos()
-        {
-        }
-
-        internal string Calcular(string v)
+        internal static string Calcular(string numeroArabigo)
         {
             return "I";
         }
